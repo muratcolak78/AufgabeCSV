7e699058120ce955a9da7b58d4f2a53a3a8c4a07
Murat Colak <Murat.Colak@training.lug-ag.de>
2024-05-08 13:16:37 +0200

    Projektdateien hinzufügen.

---
+++ AufgabeCSV/Program.cs
@@ -0,0 +1,100 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AufgabeCSV;
+
+
+
+
+liesCSV("C:\\Users\\ColakMurat\\Downloads\\TestDaten.csv");
+
+
+
+
+static List<data> liesCSV(string csvDosyaYolu)
+{
+    
+    // Verileri depolamak için bir liste oluştur
+
+    List<data> veriListesi = new List<data>();
+
+    Console.WriteLine("veri listesi olusturuldu");
+    try
+    {
+        Console.WriteLine("try bloguna girildi");
+        using (StreamReader reader = new StreamReader(csvDosyaYolu))
+        {
+            string[] veriler;
+            while (!reader.EndOfStream)
+            {
+                string satir = reader.ReadLine();
+                veriler = satir.Split(';');
+               
+                if (veriler.Length < 15)
+                {
+                    string sütun1 = veriler[0];
+                    string sütun2 = veriler[1];
+                    string sütun3 = veriler[2];
+                    string sütun4 = veriler[3];
+                    string sütun5 = veriler[4];
+                    string sütun6 = veriler[5];
+                    string sütun7 = veriler[6];
+                    string sütun8 = veriler[7];
+                    string sütun9 = veriler[8];
+                    string sütun10 = veriler[9];
+                    string sütun11 = veriler[10];
+                    string sütun12 = veriler[11];
+                    string sütun13 = veriler[12];
+                    string sütun14 = veriler[13];
+
+                  
+                    // Verileri bir sınıf nesnesine yükle
+                    data yeniVeri = new data(sütun1, sütun2, sütun3, sütun4, sütun5, sütun6, sütun7, sütun8, sütun9, sütun10, sütun11, sütun12, sütun13, sütun14);
+                    
+                    veriListesi.Add(yeniVeri);
+                   
+                }
+            }
+        }
+
+        // Veri listesini kullanarak başka işlemler yapabilirsiniz
+        foreach (var veri in veriListesi)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("CSV dosyası okunurken bir hata oluştu: " + ex.Message);
+    }
+    return veriListesi;
+}
+
+void search(string searchPerson,string csvDosyaYolu)
+{
+    List<data> veriListesi = liesCSV(csvDosyaYolu);
+    List<data> bulunaKisiler = new List<data>();
+    try
+    {
+        for (int i = 0; i < veriListesi.Count; i++)
+        {
+            if (searchPerson.Equals(veriListesi[i].Vorname))
+            {
+                bulunaKisiler.Add(veriListesi[i]);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("kisi aramasinda hata meydana geldi");
+    }
+
+    if (bulunaKisiler.Count>0)
+    {
+        foreach (var vksi in bulunaKisiler)
+        {
+
+        }
+    }else Console.WriteLine(" aranan kisi listede yok");
+
+
+}
+
---
+++ AufgabeCSV/data.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufgabeCSV
+{
+    public class data
+    {
+        public data(string nr, string anrede, string titel, string vorname, string nachname, string geburtsdatum, string straße, string hausnummer, string postleitzahl, string stadt, string telefon, string mobil, string eMail, string newsletter)
+        {
+            Nr = nr;
+            Anrede = anrede;
+            Titel = titel;
+            Vorname = vorname;
+            Nachname = nachname;
+            Geburtsdatum = geburtsdatum;
+            Straße = straße;
+            Hausnummer = hausnummer;
+            Postleitzahl = postleitzahl;
+            Stadt = stadt;
+            Telefon = telefon;
+            Mobil = mobil;
+            EMail = eMail;
+            Newsletter = newsletter;
+        }
+
+        public string Nr { get; set; }
+        public string Anrede { get; set; }
+        public string Titel { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Geburtsdatum { get; set; }
+        public string Straße { get; set; }
+        public string Hausnummer { get; set; }
+        public string Postleitzahl { get; set; }
+        public string Stadt { get; set; }
+        public string Telefon { get; set; }
+        public string Mobil { get; set; }
+        public string EMail { get; set; }
+        public string Newsletter { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Nr,-5} {Anrede,-10} {Titel,-10} {Vorname,-15} {Nachname,-15} {Geburtsdatum,-12:dd.MM.yyyy} {Straße,-20} {Hausnummer,-10} {Postleitzahl,-10} {Stadt,-15} {Telefon,-15} {Mobil,-15} {EMail,-30} {Newsletter}";
+        }
+
+    }
+}
